eb82d7bbc2146e1d3ef632aa84711d7457c45511
zrotof <manduel21@gmail.com>
2022-05-18 04:44:22 +0200

    removing amadeus

--- config/dotEnvConfig.js
+++ config/dotEnvConfig.js
@@ -3,11 +3,7 @@ dotenv.config();
 
 
 module.exports ={
-    port : process.env.PORT,
-    apiKey : process.env.API_KEY,
-    apiSecret : process.env.API_SECRET,
-    baseSearchF : process.env.BASE_SEARCHF,
-    baseSearchA: process.env.BASE_SEARCHA,
+    port : process.env.PORT || 3000,
     mailgunApiKey: process.env.MAILGUN_API_KEY,
     mailgunDomain: process.env.MAILGUN_DOMAIN,
     evEmail: process.env.ELITES_VOYAGES_EMAIL
--- server.js
+++ server.js
@@ -1,29 +1,19 @@
-const {port,baseSearchF} = require('./config/dotEnvConfig.js');
+const {port} = require('./config/dotEnvConfig.js');
 
 //Import du framework express
 const express = require('express');
 const app = express();
-
-//import de la classe qui permet de générer l'http header 
-const HttpHeader = require("./services/createHttpHeaderRequestService");
 
 const bodyParser = require('body-parser')
 
 //Importing cors
 const cors = require("./cors");
 
-const { required } = require('nodemon/lib/config');
-//Appel du module de configuration
-
 app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req,res)=>{ 
-
-    
         res.send("Le monde chico et tout ce qu'il ya dedans");
-
-  
 });
 
 //calling to the mail route
@@ -31,33 +21,6 @@ app.use('/mail', require('./routes/mail/mailRoutes'));
   
 //calling to the tour route
 app.use('/tourisme', require('./routes/tourism/tourRoutes'));
-  
 
-
-
-//On appelle la route des aéroports en lui donnant le httpheader existant
-app.use('/airports', function (req,res,next) { 
- 
-  new HttpHeader().initialiseHeader().then( resp =>{
-    req.body =resp;
-     next();
-  })
-  },require('./routes/airportsSearchRoutes'));
-
-
-//On appelle la route des aéroports en lui donnant le httpheader existant
-app.use('/flights', function (req,res,next) { 
- 
-  
-new HttpHeader().initialiseHeader().then( resp =>{
-  req.body =resp;
-   next();
-})
-
-
-  },require('./routes/flightsSearchRoutes'));
-
-
-app.listen(port, () =>console.log(`Your port is ${port} ...`));
-
+app.listen(port, '0.0.0.0', ()=>console.log(`Listening on port : ${port} ...`));
 
